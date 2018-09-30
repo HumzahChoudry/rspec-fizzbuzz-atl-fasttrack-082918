192f0715c1c8b735c65d83e040498b456e28d3e2
HumzahChoudry <humzah.choudry@gmail.com>
2018-09-30 14:07:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,28 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if ((number % 3 == 0) && (number % 5 == 0))
+      return "FizzBuzz"
+  elsif number % 5 == 0
+    return "Buzz"
+    elsif number % 3 == 0 
+      return "Fizz"
+  end
+end
+
+def fizzbuzz(number)
+  return_string = ""
+  if number % 3 == 0
+    return_string += "Fizz"
+  end
+  
+  if number % 5 == 0 
+    return_string += "Buzz"
+  end
+  
+  if return_string == ""
+    return nil
+  end
+  
+  return_string
+end
